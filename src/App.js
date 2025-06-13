f8bf0bdbe9510400c2a0360f1fd0f5540348ce6b
Marc Alex Crasto <mcrasto2@uwo.ca>
2025-06-13 17:46:57 +0300

    Substituted functions for Classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  } 
 }
+
 
 export default App;
