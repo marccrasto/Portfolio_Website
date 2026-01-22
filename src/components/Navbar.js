a0fe5c5028784e3d751601d509adc015e3b686ac
Marc Alex Crasto <mcrasto2@uwo.ca>
2026-01-22 01:25:02 -0500

    COMPLETED: Fixed 404

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,11 +13,11 @@ class Navbar extends React.Component {
         return (
             <div id="navbar" className={`navbar ${this.props.visibility ? 'visible' : ''}`}>
                 <ul>
-                    <Link to="/" ><a><li>Home</li></a></Link>
-                    <Link to="/about"><a><li>About Me</li></a></Link>
-                    <Link to="/projects"><a><li>Club Projects</li></a></Link>
-                    <Link to="/academic"><a><li>Academic Projects</li></a></Link>
-                    <Link to="/resume"><a><li>Resume</li></a></Link>
+                    <Link to="/" onClick={this.props.onClick}><a><li>Home</li></a></Link>
+                    <Link to="/about" onClick={this.props.onClick}><a><li>About Me</li></a></Link>
+                    <Link to="/projects" onClick={this.props.onClick}><a><li>Club Projects</li></a></Link>
+                    <Link to="/academic" onClick={this.props.onClick}><a><li>Academic Projects</li></a></Link>
+                    <Link to="/resume" onClick={this.props.onClick}><a><li>Resume</li></a></Link>
                 </ul>
             </div>
         );
